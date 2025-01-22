39a9dd0bc5817c3dc78f2b1e1bb3cc902bb1ed98
Satyam Sagar <satyamsagar30@gmail.com>
2025-01-22 11:55:58 +0530

    backend setup with task and project endpoints

---
+++ server/src/index.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import projectRoutes from "./routes/projectRoutes";
+import taskRoutes from "./routes/taskRoutes";
+
+/** Configurations */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* Routes */
+app.get("/", (req, res) => {
+  res.send("This is home route");
+});
+
+app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
+
+/* SERVER */
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, () => {
+  console.log(`Server running on part ${port}`);
+});
